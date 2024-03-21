8507fa2dc54892bc6694eb32ff2edba2d2be73b6
tiagoazz <150260237+tiagoazz@users.noreply.github.com>
2024-03-21 15:00:50 -0300

    Repositorio 2

--- Data/Cliente.cs
+++ Data/Cliente.cs
@@ -7,7 +7,12 @@ using System.Threading.Tasks;
 namespace Data
 {
     public class Cliente
-    { 
-        public int CodigoCliente { get; set; } 
+    {
+        public int CodigoCliente { get; set; }
+        public string Nome { get; set; }
+        public string Profissao { get; set; }
+        public string Setor { get; set;}
+        public string Obs { get; set; }
+
     }
 }
